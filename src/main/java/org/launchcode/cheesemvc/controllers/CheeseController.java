17e15489267412eb8698559472a164dc6f64fa46
rarose67 <robertrosestl67@gmail.com>
2018-05-18 22:54:06 -0500

    Finished studio 3.4

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,19 +1,47 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    //static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
+
+    public static boolean isAlpha(String text)
+    {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        char[] charactersInText;
+        String character;
+        String error;
+        boolean is_lower = true;
+        boolean is_upper = true;
+        boolean result = true;
+
+        charactersInText = text.toCharArray();
+
+        for (int i=0; i < charactersInText.length; i++)
+        {
+            character = Character.toString(charactersInText[i]);
+            is_lower = (alphabet.indexOf(character) >= 0);
+            is_upper = (alphabet.toUpperCase().indexOf(character) >= 0);
+
+            if (!is_upper && !is_lower && !(character.equals(" ")))
+            {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
 
     @RequestMapping(value = "")
     public String index(Model model)
@@ -30,18 +58,56 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model)
+    public String displayAddCheeseForm(Model model, HttpServletRequest request)
     {
         String title ="Add a Cheese";
+        String error = request.getParameter("error");
         model.addAttribute("title", title);
 
+        if (error != null)
+        {
+            model.addAttribute("error", error);
+            //System.out.println("\n" + error + "\n" + error.length() + "\n");
+        }
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName)
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription)
     {
-        cheeses.add(cheeseName);
+        boolean alpha = CheeseController.isAlpha(cheeseName);
+        String error;
+
+        if(cheeseName.equals(""))
+        {
+            error = "The Cheese name is required";
+            return "redirect:add?error=" + error;
+        }
+        else if(!alpha)
+        {
+            error = "The Cheese name must be Alphabetic";
+            return "redirect:add?error=" + error;
+        }
+        else {
+            cheeses.put(cheeseName, cheeseDescription);
+            return "redirect:";
+        }
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model)
+    {
+        String title ="Remove a Cheese";
+        model.addAttribute("title", title);
+        model.addAttribute("cheeses", cheeses);
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam String cheeseName)
+    {
+        cheeses.remove(cheeseName);
 
         return "redirect:";
     }
